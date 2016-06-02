6ee821ac65feba4ec39db54f4724fd1d2deba269
steveoc64 <steveoc64@gmail.com>
2016-06-02 14:57:16 +0930

    sample data

--- treeform.go
+++ treeform.go
@@ -187,104 +187,130 @@ func (f *TreeForm) generateTemplate(name string) *temple.Template {
 
 		src += `
 	    <ul class="css-treeview">
-        <li><input type="checkbox" id="item-0" /><label for="item-0">This Folder is Closed By Default</label>
+        <li><input type="checkbox" id="item-0" />
+        		<label for="item-0">Bracket B4/5</label>
             <ul>
-                <li><input type="checkbox" id="item-0-0" /><label for="item-0-0">Ooops! A Nested Folder</label>
-                    <ul>
-                        <li><input type="checkbox" id="item-0-0-0" /><label for="item-0-0-0">Look Ma - No Hands!</label>
-                            <ul>
-                                <li><a href="./">First Nested Item</a></li>
-                                <li><a href="./">Second Nested Item</a></li>
-                                <li><a href="./">Third Nested Item</a></li>
-                                <li><a href="./">Fourth Nested Item</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="./">Item 1</a></li>
-                        <li><a href="./">Item 2</a></li>
-                        <li><a href="./">Item 3</a></li>
-                    </ul>
+                <li><input type="checkbox" id="item-0-0" />
+                	<label for="item-0-0">Lower Crop Blade</label>
+                	<ul>
+		                <li><input type="checkbox" id="item-1-0-0" />
+		                	<label for="item-1-0-0">Consumables</label>
+		                	<ul>
+				                <li>Sharpening Stone</li>
+				                <li>Oil</li>
+				                <li>Razor Blades</li>
+		                	</ul>
+		                </li>
+		                <li>Part 1</li>
+		                <li>Part 2</li>
+		                <li>Part 3</li>
+		                <li>Part 4</li>
+		                <li>Part 5</li>
+                	</ul>
                 </li>
-                <li><input type="checkbox" id="item-0-1" /><label for="item-0-1">Yet Another One</label>
-                    <ul>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                    </ul>
+
                 </li>
-                <li><input type="checkbox" id="item-0-2" disabled="disabled" /><label for="item-0-2">Disabled Nested Items</label>
-                    <ul>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                    </ul>
+                <li><input type="checkbox" id="item-0-1" />
+                	<label for="item-0-1">Upper Crop Blade</label>
+                	<ul>
+		                <li><input type="checkbox" id="item-1-0-0" />
+		                	<label for="item-1-0-0">Part 1</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-1" />
+		                	<label for="item-1-0-1">Part 2</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-2" />
+		                	<label for="item-1-0-2">Part 3</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-3" />
+		                	<label for="item-1-0-3">Part 4</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-4" />
+		                	<label for="item-1-0-4">Part 5</label>
+		                </li>
+                	</ul>
                 </li>
-                <li><a href="./">item</a></li>
-                <li><a href="./">item</a></li>
-                <li><a href="./">item</a></li>
-                <li><a href="./">item</a></li>
-        </ul>
-</li>
-<li><input type="checkbox" id="item-1" checked="checked" /><label for="item-1">This One is Open by Default...</label>
-        <ul>
-            <li><input type="checkbox" id="item-1-0" /><label for="item-1-0">And Contains More Nested Items...</label>
-                <ul>
-                    <li><a href="./">Look Ma - No Hands</a></li>
-                    <li><a href="./">Another Item</a></li>
-                    <li><a href="./">And Yet Another</a></li>
-                </ul>
-            </li>
-            <li><a href="./">Lorem</a></li>
-            <li><a href="./">Ipsum</a></li>
-            <li><a href="./">Dolor</a></li>
-            <li><a href="./">Sit Amet</a></li>
-        </ul>
-</li>
-<li><input type="checkbox" id="item-2" /><label for="item-2">Can You Believe...</label>
-        <ul>
-                <li><input type="checkbox" id="item-2-0" /><label for="item-2-0">That This Treeview...</label>
-                    <ul>
-                        <li><input type="checkbox" id="item-2-2-0" /><label for="item-2-2-0">Does Not Use Any JavaScript...</label>
-                            <ul>
-                                <li><a href="./">But Relies Only</a></li>
-                                <li><a href="./">On the Power</a></li>
-                                <li><a href="./">Of CSS3</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="./">Item 1</a></li>
-                        <li><a href="./">Item 2</a></li>
-                        <li><a href="./">Item 3</a></li>
-                    </ul>
+
                 </li>
-                <li><input type="checkbox" id="item-2-1" /><label for="item-2-1">This is a Folder With...</label>
-                    <ul>
-                        <li><a href="./">Some Nested Items...</a></li>
-                        <li><a href="./">Some Nested Items...</a></li>
-                        <li><a href="./">Some Nested Items...</a></li>
-                        <li><a href="./">Some Nested Items...</a></li>
-                        <li><a href="./">Some Nested Items...</a></li>
-                    </ul>
+            </ul>
+        </li>
+        <li><input type="checkbox" id="item-1" />
+        		<label for="item-1">Chord</label>
+            <ul>
+                <li><input type="checkbox" id="item-1-0" />
+                	<label for="item-1-0">Down Dimple</label>
+                	<ul>
+		                <li><input type="checkbox" id="item-1-0-0" />
+		                	<label for="item-1-0-0">Part 1</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-1" />
+		                	<label for="item-1-0-1">Part 2</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-2" />
+		                	<label for="item-1-0-2">Part 3</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-3" />
+		                	<label for="item-1-0-3">Part 4</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-4" />
+		                	<label for="item-1-0-4">Part 5</label>
+		                </li>
+                	</ul>
                 </li>
-                <li><input type="checkbox" id="item-2-2" disabled="disabled" /><label for="item-2-2">Disabled Nested Items</label>
-                    <ul>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                        <li><a href="./">item</a></li>
-                    </ul>
+                <li><input type="checkbox" id="item-1-1" />
+                	<label for="item-1-1">Tie Down Slot</label>
+                	<ul>
+		                <li><input type="checkbox" id="item-1-0-0" />
+		                	<label for="item-1-0-0">Part 1</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-1" />
+		                	<label for="item-1-0-1">Part 2</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-2" />
+		                	<label for="item-1-0-2">Part 3</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-3" />
+		                	<label for="item-1-0-3">Part 4</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-4" />
+		                	<label for="item-1-0-4">Part 5</label>
+		                </li>
+                	</ul>
+                </li>
+                <li><input type="checkbox" id="item-1-2" />
+                	<label for="item-1-2">Up Dimple</label>
+                	<ul>
+		                <li><input type="checkbox" id="item-1-0-0" />
+		                	<label for="item-1-0-0">Part 1</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-1" />
+		                	<label for="item-1-0-1">Part 2</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-2" />
+		                	<label for="item-1-0-2">Part 3</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-3" />
+		                	<label for="item-1-0-3">Part 4</label>
+		                </li>
+		                <li><input type="checkbox" id="item-1-0-4" />
+		                	<label for="item-1-0-4">Part 5</label>
+		                </li>
+                	</ul>
+                </li>
+                <li><input type="checkbox" id="item-1-3" />
+                	<label for="item-1-3">Half Notch</label>
+                </li>
+                <li><input type="checkbox" id="item-1-4" />
+                	<label for="item-1-4">Full Notch</label>
+                </li>
+                <li><input type="checkbox" id="item-1-5" />
+                	<label for="item-1-5">Right Angle Guillo</label>
+                </li>
+                <li><input type="checkbox" id="item-1-6" />
+                	<label for="item-1-6">Straight Guillo</label>
+                </li>
+                <li><input type="checkbox" id="item-1-7" />
+                	<label for="item-1-7">Left Angle Guillo</label>
                 </li>
             </ul>
         </li>
